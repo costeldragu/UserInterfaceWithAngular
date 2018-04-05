17af83c18b4cf979960c8a1938f3d59b83551891
Dragu Costel <costel.dragu@endava.com>
2018-04-05 16:15:03 +0300

    Updated project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { EventsComponent } from './events-app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
+  declarations: [
+    EventsComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsComponent]
 })
 export class AppModule { }
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'events-app',
+  template:`
+      <h2>Hellow World </h2>
+      <img src="/assets/images/basic-shield.png" />
+  
+  `,
 })
-export class AppComponent {
-  title = 'app';
+export class EventsComponent {
+  title = 'Events app component';
 }
